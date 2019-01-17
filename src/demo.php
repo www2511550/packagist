85aef2d953d4d7d82d82fa71d5d06c2c22b5cfe6
cc <cc@qq.com>
2019-01-17 10:43:37 +0800

    first packagist

---
+++ src/demo.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace TestDemo;
+
+/**
+* demo
+*/
+class Demo
+{
+	
+	public static function world()
+	{
+		return 'Hello World!';
+	}
+}
+
+
+ ?>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php  
+require_once __DIR__ . '/vendor/autoload.php';  
+
+use TestDemo\Demo;
+
+echo Demo::world();
